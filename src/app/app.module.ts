929389cb5116c7732041643f61be77df0ad63d86
raja.arumugam@ba.com-at-929874591255 <JdagEx3xMX/TNkoXZDjOQieV5Viex1clYJD6j2NYqfQ=>
2020-03-22 17:33:02 +0000

    added animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
+import {BannerComponentComponent} from './banner-component/banner-component.component';
+import {CompanyComponent} from './company/company.component';
+import {SkillsetComponent} from './skillset/skillset.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {EducationComponent} from './education/education.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CompanyComponent,
+    SkillsetComponent,
+    PortfolioComponent,
+    EducationComponent,
+    FooterComponent,
+    BannerComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
